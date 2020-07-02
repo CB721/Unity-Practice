0a9661fb6693a2c0a6f71b2eb0676a7291bcefd3
Clint Brodar <clintbrodar@gmail.com>
2020-07-02 15:42:44 -0700

    Auto focus object on target

---
+++ Rotations/Assets/Aim.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aim : MonoBehaviour
+{
+    // reference to the focus cube
+    [SerializeField]
+    private Transform _focusCube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // direction = destination - source
+        // should return a vector3
+        Vector3 direction = _focusCube.position - transform.position;
+        Debug.DrawRay(transform.position, direction, Color.green);
+        // access our current location = Quaternion look rotation
+        transform.rotation = Quaternion.LookRotation(direction);
+    }
+}
---
+++ Rotations/Assets/SpawnCube.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCube : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject cubePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(cubePrefab, new Vector3(0, 5, 0), Quaternion.Euler(45, 45, 0));
+        }
+    }
+}
