56d2a27ed51fce6e6aa48d2c7f40d5a6ba244784
Clint Brodar <clintbrodar@gmail.com>
2020-07-07 11:43:19 -0700

    Print out loop exercise

---
+++ Loops/Assets/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // create a program that prints out 0 - 10 and then prints out only even numbers to 20 and then only odd numbers to 30
+
+    void Start()
+    {
+        for (int i = 0; i < 30; i++)
+        {
+            if (i <= 10)
+            {
+                Debug.Log(i);
+            }
+            else if (i > 10 && i <= 20 && i % 2 == 0)
+            {
+                Debug.Log(i);
+            }
+            else if (i > 20 && i <= 30 && i % 2 == 1)
+            {
+                Debug.Log(i);
+            }
+        }
+    }
+
+}
