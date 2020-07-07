0aa77a6db83ec72c8a4731e41c3fe0d902635820
Clint Brodar <clintbrodar@gmail.com>
2020-07-07 11:13:08 -0700

    Loop coroutine

---
+++ Loops/Assets/Apples.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apples : MonoBehaviour
+{
+    private int _apples;
+    private void Start() 
+    {
+        StartCoroutine(AddApplesRoutine());
+    }
+
+    IEnumerator AddApplesRoutine()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Debug.Log($"There are {_apples} apples");
+            _apples++;
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+}
