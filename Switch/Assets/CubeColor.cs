602c17aa25836f9225672d6306f322530d84d1e6
Clint Brodar <clintbrodar@gmail.com>
2020-07-07 10:19:03 -0700

    Cube color switch

---
+++ Switch/Assets/CubeColor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeColor : MonoBehaviour
+{
+    public GameObject cube;
+
+    // create a program that turns the cube a different color based on user input
+    // 1 key = blue
+    // 2 key = red
+    // 3 key = green
+    // 4 key = black
+    void Update()
+    {
+        string _keyPressed = Input.inputString;
+        switch (_keyPressed)
+        {
+            case "1":
+                cube.GetComponent<Renderer>().material.color = Color.blue;
+                break;
+            case "2":
+                cube.GetComponent<Renderer>().material.color = Color.red;
+                break;
+            case "3":
+                cube.GetComponent<Renderer>().material.color = Color.green;
+                break;
+            case "4":
+                cube.GetComponent<Renderer>().material.color = Color.black;
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ Switch/Assets/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private int points;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            points = 50;
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            points = 100;
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            points = 0;
+        }
+        // if points are greater or equal to 50
+        // print points to the console
+        // if points are 100
+        // print points to the console
+        // otherwise print 'You need 50 or 100 points to see a message'
+        switch (points)
+        {
+            case 50:
+                Debug.Log($"You have {points} points");
+                break;
+            case 100:
+                Debug.Log($"You have {points} points");
+                break;
+            default:
+                Debug.Log("You need 50 or 100 points to see a message");
+                break;
+        }
+    }
+}
