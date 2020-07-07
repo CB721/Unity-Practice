6fe2acbd746093c9b6f281abbdf6507d235614f9
Clint Brodar <clintbrodar@gmail.com>
2020-07-07 13:49:44 -0700

    Random value from arrays

---
+++ Arrays/Assets/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // select a random age and random name
+    // print these values
+    public int[] ages;
+    public string[] names;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int randomAge = ages[Random.Range(0, ages.Length)];
+        string randomName = names[Random.Range(0, names.Length)];
+        Debug.Log(randomAge);
+        Debug.Log(randomName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
