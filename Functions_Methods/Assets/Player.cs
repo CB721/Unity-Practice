2e5ca2ebed41c2f3f5532676543f736383069a60
Clint Brodar <clintbrodar@gmail.com>
2020-07-10 11:56:23 -0700

    Change position on key press - wip

--- Functions_Methods/Assets/Player.cs
+++ Functions_Methods/Assets/Player.cs
@@ -6,8 +6,15 @@ public class Player : MonoBehaviour
 {
     public int health = 100;
     public int attack = 5;
-    // Start is called before the first frame update
-    
+    [SerializeField]
+    private float _horSpeed = 0;
+    [SerializeField]
+    private float _verSpeed = 0;
+    [SerializeField]
+    private float _horizontal;
+    [SerializeField]
+    private float _vertical;
+
 
     // Update is called once per frame
     void Update()
@@ -16,6 +23,22 @@ public class Player : MonoBehaviour
         {
             Damage(attack);
         }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            ChangePosition(new Vector3(0, 0.5f, -245));
+        }
+        _horizontal = Input.GetAxis("Horizontal");
+        _vertical = Input.GetAxis("Vertical");
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            StartCoroutine(ChangeSpeed(true, true));
+        }
+        else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            StartCoroutine(ChangeSpeed(true, false));
+        }
+        Debug.Log($"Your horizontal speed is {_horSpeed}");
+        Debug.Log($"Your vertical speed is {_verSpeed}");
     }
 
     private void Damage(int amount)
@@ -32,5 +55,44 @@ public class Player : MonoBehaviour
     {
         health = 100;
         Debug.Log("You have been revived!");
+    }
+    private void ChangePosition(Vector3 pos)
+    {
+        transform.position = pos;
+    }
+    private void ChangeTranslate(Vector3 pos, bool isHorizontal)
+    {
+        if (isHorizontal)
+        {
+            transform.Translate(pos * _horSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(pos * _verSpeed * Time.deltaTime);
+        }
+    }
+    IEnumerator ChangeSpeed(bool isHorizontal, bool isForwards)
+    {
+        // for horizontal values, forwards = right
+        if (isHorizontal && isForwards)
+        {
+            while (_horSpeed < 10)
+            {
+                _horSpeed += 1;
+                ChangeTranslate(new Vector3(_horizontal, 0, 0), true);
+                ChangeTranslate(new Vector3(0, _vertical, 0), true);
+                yield return new WaitForSeconds(1.0f);
+            }
+        }
+        else if (isHorizontal)
+        {
+            while (_horSpeed > 0)
+            {
+                _horSpeed -= 1;
+                ChangeTranslate(new Vector3(_horizontal, 0, 0), false);
+                ChangeTranslate(new Vector3(0, _vertical, 0), true);
+                yield return new WaitForSeconds(1.0f);
+            }
+        }
     }
 }
