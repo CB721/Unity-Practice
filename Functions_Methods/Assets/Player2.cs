c4faa44eef7a5bac98e7f633c5a2ac9214e49a62
Clint Brodar <clintbrodar@gmail.com>
2020-07-10 13:29:50 -0700

    Random position on key press

---
+++ Functions_Methods/Assets/Player2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2 : MonoBehaviour
+{
+    // create a program that places the player in a random position at the start
+    // they should also move to a new random position whenever space is pressed
+    
+    void Start()
+    {
+        transform.position = RandomPos();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.position = RandomPos();
+        }
+    }
+    private Vector3 RandomPos()
+    {
+        return new Vector3(Random.Range(-10, 10), Random.Range(0, 10), -220);
+    }
+}
