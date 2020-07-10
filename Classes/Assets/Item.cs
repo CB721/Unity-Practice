b7541e84ac121671811b63c0f8d24c402e10492c
Clint Brodar <clintbrodar@gmail.com>
2020-07-10 14:47:48 -0700

    Serializeable class

---
+++ Classes/Assets/Item.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Item
+{
+    public string name;
+    public int id;
+    public string desc;
+    public Item(string name, int id, string desc)
+    {
+        this.name = name;
+        this.id = id;
+        this.desc = desc;
+    }
+}
---
+++ Classes/Assets/ItemDB.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDB : MonoBehaviour
+{
+    public Item[] items;
+    public Item sword;
+    public Item hammer;
+    public Item bread;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sword = new Item("Sword", 1, "So sharp and pointy");
+        hammer = new Item("Hammer", 2, "Dull but powerful");
+        bread = CreateItem("Bread", 3, "Fulling whole grain bread");
+    }
+    private Item CreateItem(string name, int id, string desc)
+    {
+        return new Item(name, id, desc);
+    }
+    
+}
