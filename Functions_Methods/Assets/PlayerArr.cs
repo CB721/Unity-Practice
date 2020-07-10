5d0a430802e6f1883ca4f769125cab14746e8be8
Clint Brodar <clintbrodar@gmail.com>
2020-07-10 12:28:42 -0700

    Dynamic number of players given random color

---
+++ Functions_Methods/Assets/PlayerArr.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerArr : MonoBehaviour
+{
+    // create a program that finds all "players"
+    // each player should then be given a random color
+    public GameObject[] players;
+    // Start is called before the first frame update
+    void Start()
+    {
+        players = GetAllPlayers();
+    }
+    GameObject[] GetAllPlayers()
+    {
+        GameObject[] allPlayers = GameObject.FindGameObjectsWithTag("Player");
+        for (int i = 0; i < allPlayers.Length; i++)
+        {
+            allPlayers[i].GetComponent<MeshRenderer>().material.color = new Color(Random.value, Random.value, Random.value);
+        }
+        return allPlayers;
+    }
+}
