1c74a13bfea286a2a34b85de10d9b331d4667f44
Clint Brodar <clintbrodar@gmail.com>
2020-07-10 22:18:34 -0700

    Spells cast

---
+++ Classes/Assets/Spell.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Spell
+{
+    public string name;
+    public int levelRequired;
+    public int itemIDRequired;
+    public int expGained;
+
+    public Spell(string name, int levelRequired, int itemIDRequired, int expGained)
+    {
+        this.name = name;
+        this.levelRequired = levelRequired;
+        this.itemIDRequired = itemIDRequired;
+        this.expGained = expGained;
+    }
+    public void Cast()
+    {
+        Debug.Log($"{name} spell cast");
+    }
+}
