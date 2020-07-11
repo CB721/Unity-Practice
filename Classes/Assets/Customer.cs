ddb0a27f18fb1b066c19ff8d31f620f5582d76ea
Clint Brodar <clintbrodar@gmail.com>
2020-07-10 21:53:53 -0700

    Customer 'database' class

---
+++ Classes/Assets/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Customer
+{
+    public string firstName;
+    public string lastName;
+    public int id;
+    public int age;
+    public char gender;
+    public string occupation;
+    public System.DateTime joinedAt;
+    public Customer(string firstName, string lastName, int id, int age, char gender, string occupation, System.DateTime joinedAt)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+        this.age = age;
+        this.gender = gender;
+        this.occupation = occupation;
+        this.joinedAt = joinedAt;
+    }
+
+}
---
+++ Classes/Assets/CustomerDB.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerDB : MonoBehaviour
+{
+    public Customer charlie;
+    public Customer snoopy;
+    public Customer linus;
+    // Start is called before the first frame update
+    void Start()
+    {
+        charlie = CreateCustomer("Charlie", "Brown", 0, 8, 'M', "Kicker");
+        snoopy = CreateCustomer("Snoopy", "Brown", 1, 3, 'M', "Dog");
+        linus = CreateCustomer("Linus", "van Pelt", 2, 8, 'M', "Blanket Holder");
+    }
+    private Customer CreateCustomer(string firstName, string lastName, int id, int age, char gender, string occupation)
+    {
+        return new Customer(firstName, lastName, id, age, gender, occupation, System.DateTime.Now);
+    }
+}
