cb79d4f45bf3938a8d6cf24aa7f32ee163b47fcb
Clint Brodar <clintbrodar@gmail.com>
2020-07-14 10:39:55 -0700

    Bank system inheritance

---
+++ Classes/Assets/Bank.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Bank
+{
+    public string branchName;
+    public string location;
+    public string cashInVault;
+    public void CheckBalance()
+    {
+        Debug.Log($"Checking balance at {branchName} bank");
+    }
+    public void Withdraw()
+    {
+        Debug.Log($"Withdrawing money from {branchName} bank");
+    }
+    public void Deposit()
+    {
+        Debug.Log($"Depositing money to {branchName} bank");
+    }
+}
---
+++ Classes/Assets/FederalCreditUnion.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class FederalCreditUnion : Bank
+{
+    public int availableCashToLend;
+    public void ApproveLoan()
+    {
+        Debug.Log("You are awarded a loan!");
+    }
+}
