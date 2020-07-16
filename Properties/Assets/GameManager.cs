fa0de42e597706c497e2a7cfc27c8ebc2256e06b
Clint Brodar <clintbrodar@gmail.com>
2020-07-16 11:37:59 -0700

    Auto properties

--- Properties/Assets/GameManager.cs
+++ Properties/Assets/GameManager.cs
@@ -4,42 +4,43 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private bool isGameOver;
-    public bool IsGameOver
-    {
-        get
-        {
-            return isGameOver;
-        }
-        set
-        {
-            if (value)
-            {
-                Debug.Log("The game is over");
-            }
-            else 
-            {
-                Debug.Log("The game is still going");
-            }
-            isGameOver = value;
-        }
-    }
-    // Start is called before the first frame update
+    // private bool isGameOver;
+    // public bool IsGameOver
+    // {
+    //     get
+    //     {
+    //         return isGameOver;
+    //     }
+    //     set
+    //     {
+    //         if (value)
+    //         {
+    //             Debug.Log("The game is over");
+    //         }
+    //         else 
+    //         {
+    //             Debug.Log("The game is still going");
+    //         }
+    //         isGameOver = value;
+    //     }
+    // }
+
+    public bool IsGameOver { get; private set; }
     void Start()
     {
-        isGameOver = false;
+        // isGameOver = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            IsGameOver = true;
-        }
-        if (Input.GetKeyDown(KeyCode.G))
-        {
-            IsGameOver = false;
-        }
+        // if (Input.GetKeyDown(KeyCode.Space))
+        // {
+        //     IsGameOver = true;
+        // }
+        // if (Input.GetKeyDown(KeyCode.G))
+        // {
+        //     IsGameOver = false;
+        // }
     }
 }
