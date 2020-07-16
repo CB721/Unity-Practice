172f49d0fdf2107e9b14543eb31cbe6785f0a22b
Clint Brodar <clintbrodar@gmail.com>
2020-07-16 11:28:09 -0700

    Getters and setters

---
+++ Properties/Assets/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private bool isGameOver;
+    public bool IsGameOver
+    {
+        get
+        {
+            return isGameOver;
+        }
+        set
+        {
+            if (value)
+            {
+                Debug.Log("The game is over");
+            }
+            else 
+            {
+                Debug.Log("The game is still going");
+            }
+            isGameOver = value;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGameOver = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            IsGameOver = true;
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            IsGameOver = false;
+        }
+    }
+}
