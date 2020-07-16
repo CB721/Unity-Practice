f4c1aa7c43e808dc0a241fdabf358e686b827ded
Clint Brodar <clintbrodar@gmail.com>
2020-07-16 10:18:31 -0700

    Instance vs static variables

---
+++ StaticTypes/Assets/InstanceTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item4 {
+    public string name;
+    public int itemID;
+    // each item created will increment the count
+    public static int itemCount;
+    public Item4(string name, int ID)
+    {
+        this.name = name;
+        this.itemID = ID;
+        itemCount++;
+    }
+}
+
+public class InstanceTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Item4 water = new Item4("water", 1);
+        Debug.Log($"Current item count: {Item4.itemCount}");
+        Item4 phone = new Item4("phone", 2);
+        Debug.Log($"Current item count: {Item4.itemCount}");
+    }
+}
