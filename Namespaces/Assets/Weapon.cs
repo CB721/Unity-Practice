cba89afe271b2e814486e78c4b7de9acda9897ee
Clint Brodar <clintbrodar@gmail.com>
2020-07-22 13:49:02 -0700

    Namespaces

---
+++ Namespaces/Assets/Weapon.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+// import classes from our own created namespace
+using WeaponPack;
+
+public class Weapon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        WeaponPack.Weapon = new WeaponPack.Weapon();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
