10086733bb0e5b53c42d2c6e874e0c4e1492f0ea
Clint Brodar <clintbrodar@gmail.com>
2020-07-23 12:57:46 -0700

    Level selector

---
+++ Enums/Assets/SelectDifficulty.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectDifficulty : MonoBehaviour
+{
+    public enum LevelSelector
+    {
+        Easy,
+        Normal,
+        Hard,
+        Expert
+    }
+    public LevelSelector currLevel;
+    void Start()
+    {
+        Debug.Log($"You selected the {currLevel} level!");
+    }
+}
