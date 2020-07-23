a9128348b1928551a973f519f8832192b624a812
Clint Brodar <clintbrodar@gmail.com>
2020-07-23 13:22:07 -0700

    Enums in custom class

---
+++ Enums/Assets/EnumItem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class EnumItem
+{
+    public string name;
+    public int ID;
+    public Sprite icon;
+    public enum ItemType
+    {
+        None,
+        Weapon,
+        Consumable
+    }
+    public ItemType itemType;
+    public void Action()
+    {
+        switch (itemType)
+        {
+            case ItemType.Weapon:
+                Debug.Log("This is a weapon");
+                break;
+            case ItemType.Consumable:
+                Debug.Log("This is a consumable");
+                break;
+            default:
+                return;
+        }
+    }
+}
---
+++ Enums/Assets/TestDB.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDB : MonoBehaviour
+{
+    public List<EnumItem> ItemDB = new List<EnumItem>();
+    void Start()
+    {
+        ItemDB[0].Action();
+    }
+}
